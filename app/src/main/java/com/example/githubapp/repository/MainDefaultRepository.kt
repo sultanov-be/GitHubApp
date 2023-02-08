fd1036a1719bd78c68e9fd3f4f015026e5393297
Bektur <yasultanovbektur@yandex.ru>
2023-02-08 14:04:33 +0600

    Added repo, utils.

---
+++ app/src/main/java/com/example/githubapp/repository/MainDefaultRepository.kt
@@ -0,0 +1,26 @@
+package com.example.githubapp.repository
+
+import com.example.githubapp.data.ApiService
+import com.example.githubapp.data.model.ReposList
+import com.example.githubapp.utils.Resource
+
+import javax.inject.Inject
+
+class MainDefaultRepository @Inject constructor(
+    private val api: ApiService
+) : MainRepository {
+
+    override suspend fun getRepos(): Resource<ReposList> {
+        return try {
+            val response = api.getAllRepos()
+            val result = response.body()
+            if(response.isSuccessful && result != null) {
+                Resource.Success(result)
+            } else {
+                Resource.Error(response.message())
+            }
+        } catch(e: Exception) {
+            Resource.Error(e.message ?: "An error occurred")
+        }
+    }
+}
