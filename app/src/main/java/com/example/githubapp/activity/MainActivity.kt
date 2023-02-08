afad3420cdf73253dfef1cd6630d9b0822157bbb
Bektur <yasultanovbektur@yandex.ru>
2023-02-08 09:21:18 +0600

    Added NavComponent.

--- app/src/main/java/com/example/githubapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/githubapp/activity/MainActivity.kt
@@ -2,15 +2,29 @@ package com.example.githubapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.githubapp.R
 import com.example.githubapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupNavController()
+    }
+
+    private fun setupNavController() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        navController.graph = navController.navInflater.inflate(R.navigation.nav_graph)
     }
 }
