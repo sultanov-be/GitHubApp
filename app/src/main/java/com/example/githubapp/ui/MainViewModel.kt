10d19d8b2041576eb0c9a6e315f22766f9e05578
Bektur <yasultanovbektur@yandex.ru>
2023-02-09 11:04:03 +0600

    Defined scope.

--- app/src/main/java/com/example/githubapp/ui/MainViewModel.kt
+++ app/src/main/java/com/example/githubapp/ui/MainViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.githubapp.data.model.ReposList
 import com.example.githubapp.data.repository.MainRepository
+import com.example.githubapp.utils.DispatcherProvider
 import com.example.githubapp.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -13,7 +14,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val repository: MainRepository
+    private val repository: MainRepository,
+    private val dispatcher: DispatcherProvider
 ) : ViewModel(), LifecycleObserver {
 
     val reposAll: MutableLiveData<Resource<ReposList>> = MutableLiveData()
@@ -22,9 +24,8 @@ class MainViewModel @Inject constructor(
         getRepos()
     }
 
-    private fun getRepos()=viewModelScope.launch {
+    private fun getRepos() = viewModelScope.launch(dispatcher.io) {
         reposAll.postValue(Resource.Loading())
-        val response = repository.getRepos()
-        reposAll.postValue(response)
+        reposAll.postValue(repository.getRepos())
     }
 }
